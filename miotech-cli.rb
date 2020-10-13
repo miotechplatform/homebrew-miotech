ce44b8be8088e9e328cc6f1e64ba22e17b5ef51c
miotechplatform <72731773+miotechplatform@users.noreply.github.com>
2020-10-13 16:05:23 +0800

    Add files via upload

---
+++ miotech-cli.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MiotechCli < Formula
+  desc "MioTech Command Line Tools"
+  homepage ""
+  url "https://github.com/miotechplatform/miotech-cli/blob/main/miotech-cli_1.0.0.tar.gz"
+  sha256 "4b263ef48253c28b3837d4315ff422a4fdb4904288ec7019a7e2755206a51f8d"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    bin.install "miotech-cli"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test miotech-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
