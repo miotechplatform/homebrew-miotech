6505970f772515ea19183805a6958d662360598c
miotechplatform <72731773+miotechplatform@users.noreply.github.com>
2020-10-14 11:41:14 +0800

    Update miotech-cli.rb

--- miotech-cli.rb
+++ miotech-cli.rb
@@ -1,15 +1,33 @@
 class MiotechCli < Formula
-  desc "MioTech Command-Line"
-  homepage "https://miotech.com"
-  url "https://github.com/miotechplatform/miotech-cli/blob/main/miotech-cli_1.0.0.tar.gz"
-  sha256 "1d45f0615bcea9d993f20941e7c46275688cad2ad4fcbb0a4eb91b7ea0136d8a"
+  desc "MioTech Command-Line Tools"
+  homepage ""
+  url "https://github.com/miotechplatform/miotech-cli/raw/main/miotech-cli_1.0.0.tar.gz"
+  sha256 "6eb9994dc27c2d96a445b3de24fc36b97f07c6c00ac5083826477317b23d7804"
   license ""
 
+  # depends_on "cmake" => :build
+
   def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
     bin.install "miotech-cli"
   end
 
   test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test miotech-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
